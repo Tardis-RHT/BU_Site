9278e7252c8c7e2046f9167293c0c9e1d9d17f98
Annie Alig <coolahhy@gmail.com>
2017-10-13 00:39:33 +0300

    in progress

--- index.php
+++ index.php
@@ -5,5 +5,28 @@
     <?php get_template_part('mods/site-header/site', 'header'); ?>
     <h1>Hello!</h1>
     <a href="#">How are you?</a>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script>
+    jQuery(document).ready(function($){
+        // debuger begin
+        var flag = 1;
+        $('html').keydown(function(eventObject){ //отлавливаем нажатие клавиш
+            if (event.ctrlKey && event.keyCode == 71) { //если нажали Ctrl+g
+                $('html').toggleClass('debuger');
+                $('body *').each( function() {
+                if ($(this).css("display")=="block"){ 
+                    $(this).toggleClass('debuger__block').parents().removeClass('debuger__block'); }
+                });
+                if(flag == 1){
+                    $('head').append('<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/debuger.css" />');
+                    $('body').append('<div class="debuger-grid"><div></div><div></div><div></div><div></div></div>')
+                    flag++
+                }
+            }
+        });
+        //debuger end
+    });
+</script>
 </body>
 </html>
