8d751fc07c0609b23819307e83b1ac84e6339526
digvice <digvice@bionics-iMac.local>
2017-10-12 16:24:42 +0300

    Added markup for the footer

---
+++ mods/site-footer/site-footer.php
@@ -0,0 +1,23 @@
+        <!-- footer -->
+        <footer class="wrapper">
+            <div>
+                <a href="mailto:University@bionic-university.com">University@bionic-university.com</a>
+                <p>Киев, ул. Семьи Хохловых, 8</p>
+                <p>&#169; 2012 - 2017</p>       
+            </div>
+            <div>
+                <a href="tel:+380937285663">(093) 728-56-63</a>
+                <a href="tel:+380987349808">(098) 734-98-08</a>
+            </div>
+            <div>
+                <a href="#">LinkedIn</a>
+                <a href="#">Instagram</a>
+                <a href="#">Facebook</a>
+            </div>
+            <div>
+                <p>Инициатор</p>
+            </div>
+        </footer>
+        <!-- /footer -->
+    </body>
+</html>
