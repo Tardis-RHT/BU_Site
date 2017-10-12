ac45f8051b82634133471f03ec8351b7dc7e07bd
annie_alig <annie_alig@bionics-iMac-eleven.local>
2017-10-12 17:20:52 +0300

    improved gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var gcmq = require('gulp-group-css-media-queries');
+var browserSync = require('browser-sync');
+var connect = require('gulp-connect-php');
+var postcss = require('gulp-postcss');
+var cssnext = require('postcss-cssnext');
+
+var paths = {
+    css:['css/*.css','mods/*/*.css'],
+    php:['*.php', 'mods/*/*.php'],
+    dir:['./']
+};
+var plugins = [
+    cssnext({
+        browserslist: [
+            // "> 1%",
+            "Chrome >= 58",
+            "Firefox >= 45",
+            "Safari >= 9",
+            "Opera >= 45",
+            "IE 11",
+            "Edge >= 15",
+          ]
+    })
+];
+
+// gulp.task ('html', function() {
+//     return gulp.src('*.html')
+//         .pipe(browserSync.stream());
+//   });
+
+gulp.task('connect-sync', function() {
+    connect.server({}, function() {
+        browserSync({
+            proxy: '127.0.0.1:8888'
+        });
+    });
+
+    gulp.watch('**/*.php').on('change', function () {
+        browserSync.reload();
+    });
+});
+
+gulp.task('css', function () {
+    return gulp.src('css/style.css')
+        .pipe(postcss(plugins))
+        .pipe(cleanCSS()) 
+        .pipe(concat('stylemin.css'))
+        .pipe(gcmq())
+        .pipe(cleanCSS()) 
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.stream());
+});
+
+        // -->> Most comments are here in case we will need some html work:
+// gulp.task('scripts', function() {  
+//     return gulp.src('js/*')
+//         .pipe(concat('scripts.js'))
+//         .pipe(gulp.dest('js/'))
+//         .pipe(browserSync.stream());
+// });
+
+// gulp.task('watch', ['css', 'html'], function() {
+gulp.task('watch', ['css', 'connect-sync'], function() {
+    // browserSync({
+    //     server: {
+    //         baseDir: paths.dir
+    //     },
+    //     port: 8888
+    // }); 
+
+    connect.server({}, function() {
+        browserSync({
+            proxy: '127.0.0.1:8888'
+        });
+    });
+
+    gulp.watch([paths.php]).on('change', browserSync.reload);
+    gulp.watch([paths.css], ['css']);
+    // gulp.watch(["js/*"], ['js']);
+    // gulp.watch("*.html", ['html']).on('change', browserSync.reload);
+
+});
+
+gulp.task('default', ['watch']);
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php get_header(); ?>  
+<body>
+    <?php get_template_part('mods/site-header/site', 'header'); ?>
+    <h1>Hello!</h1>
+    <a href="#">How are you?</a>
+</body>
+</html>
