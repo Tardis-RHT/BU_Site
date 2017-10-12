9e06ab7253ceef35e78b2649152f4efb117e1d99
digvice <digvice@bionics-iMac.local>
2017-10-13 00:17:00 +0300

    Work in progress

--- index.php
+++ index.php
@@ -1,2 +1,56 @@
-<?php get_header(); ?>
-<?php get_template_part('mods/site-footer/site', 'footer'); ?>
+<!DOCTYPE html>
+<html>
+	<body>
+        <?php get_header(); ?>
+
+
+
+        <?php get_template_part('mods/site-footer/site', 'footer'); ?>
+	</body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script>
+	jQuery(document).ready(function($){
+		$('.post iframe').each(function(){
+			if($(this).parent().siblings().not('.inpost-wrap--big')){
+				$(this).wrap('<div class="inpost-wrap--big"></div>');
+			}
+		});
+		$('.post-aside').each(function(){
+			$(this).prev('p').css({'float':'left','width':'100%','margin-bottom':'1rem'});
+		});
+		var doit;
+		function resizedw() {
+			$('.aligncenter,.alignleft,.alignright,.inpost-wrap,.inpost-wrap--big,.post-aside').each(function() {
+				lh = 8; //base-line-height
+				th = parseFloat( $( this ).css("height"));
+				need_margin = Math.floor(th/lh +1)*lh - th;
+				if(need_margin < lh){
+					$( this ).css('padding-bottom', need_margin);
+				}
+			});
+		};
+		resizedw();
+		window.onresize = function() {
+			clearTimeout(doit);
+			doit = setTimeout(resizedw, 250);
+		};
+		// debuger begin
+		var flag = 1;
+		$('html').keydown(function(eventObject){ //отлавливаем нажатие клавиш
+			if (event.ctrlKey && event.keyCode == 71) { //если нажали Ctrl+g
+				$('html').toggleClass('debuger');
+				$('body *').each( function() {
+	            if ($(this).css("display")=="block"){ 
+	                $(this).toggleClass('debuger__block').parents().removeClass('debuger__block'); }
+	            });
+	            if(flag == 1){
+	            	$('head').append('<link rel="stylesheet" href="<?php bloginfo('template_directory');?>/css/debuger.css" />');
+	            	$('body').append('<div class="debuger-grid"><div></div><div></div><div></div><div></div></div>')
+	            	flag++
+	            }
+			}
+		});
+		//debuger end
+	});
+	</script>
+</html>
--- mods/site-footer/site-footer.php
+++ mods/site-footer/site-footer.php
@@ -1,21 +1,22 @@
         <!-- footer -->
-        <footer class="wrapper">
-            <div>
+        <footer class="wrapper footer">
+            <div class="footer__contacts">
                 <a href="mailto:University@bionic-university.com">University@bionic-university.com</a>
                 <p>Киев, ул. Семьи Хохловых, 8</p>
-                <p>&#169; 2012 - 2017</p>       
+                <p>&#169; 2012 - 2017</p>      
             </div>
-            <div>
+            <div class="footer__tel">
                 <a href="tel:+380937285663">(093) 728-56-63</a>
                 <a href="tel:+380987349808">(098) 734-98-08</a>
             </div>
-            <div>
+            <div class="footer__social" >
                 <a href="#">LinkedIn</a>
-                <a href="#">Instagram</a>
                 <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
             </div>
-            <div>
+            <div class="footer__sponsor">
                 <p>Инициатор</p>
+                <img src="<?php echo get_template_directory_uri(); ?>/img/icons/bionichill.png" alt="Bionic Hill">
             </div>
         </footer>
         <!-- /footer -->
