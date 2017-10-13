41b72b013be46c256c8623dee32ec1dd30d08057
Anna Vovchenko <coolahhy@gmail.com>
2017-10-13 12:28:55 +0300

    header with scroll, without scripts

--- mods/site-header/site-header.php
+++ mods/site-header/site-header.php
@@ -2,12 +2,9 @@
     <a href="<?php get_home_url() ?>" class="logo">
         <img src="<?php echo get_template_directory_uri() ?>/img/logo.png" alt="Bionic University" class="logo__img">
     </a>
-    <nav class="main-menu">
-        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
-    </nav>
     <ul class="languages">
         <li class="languages__item">
-            <a href="#rus">Рус</a>
+            <a href="#rus" class="languages__is-current">Рус</a>
         </li>
         <li class="languages__item">
             <a href="#ukr">Укр</a>
@@ -16,4 +13,7 @@
             <a href="#eng">Eng</a>
         </li>
     </ul>
+    <nav class="main-menu">
+        <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+    </nav>
 </header>
