cf6526012d41f6ddce25a345f22dffb725fc09a3
digvice <digvice@bionics-iMac.local>
2017-10-13 15:40:40 +0300

    Work in progress

--- mods/site-footer/site-footer.php
+++ mods/site-footer/site-footer.php
@@ -3,20 +3,22 @@
             <div class="footer__contacts">
                 <a href="mailto:University@bionic-university.com">University@bionic-university.com</a>
                 <p>Киев, ул. Семьи Хохловых, 8</p>
-                <p>&#169; 2012 - 2017</p>      
+                <p class="footer__contacts-copyright">&#169; 2012 - 2017</p>      
             </div>
             <div class="footer__tel">
                 <a href="tel:+380937285663">(093) 728-56-63</a>
                 <a href="tel:+380987349808">(098) 734-98-08</a>
             </div>
             <div class="footer__social" >
-                <a href="#">LinkedIn</a>
-                <a href="#">Facebook</a>
-                <a href="#">Instagram</a>
+                <a class="footer__social-linkedin" href="#"></a>
+                <a class="footer__social-instagram" href="#"></a>
+                <a class="footer__social-facebook" href="#"></a>
             </div>
             <div class="footer__sponsor">
-                <p>Инициатор</p>
-                <img src="<?php echo get_template_directory_uri(); ?>/img/icons/bionichill.png" alt="Bionic Hill">
+                <div class="footer__sponsor-wrap" >
+                    <p>Инициатор</p>
+                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/icons/bionichill.png" alt="Bionic Hill"></a>
+                </div>
             </div>
         </footer>
         <!-- /footer -->
