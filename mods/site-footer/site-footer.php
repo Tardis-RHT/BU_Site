fcfcda1fb8956b2e2e48bb3221fe2e16a58785e5
digvice <digvice@mail.ru>
2017-10-15 21:38:56 +0300

    fixed footer

--- mods/site-footer/site-footer.php
+++ mods/site-footer/site-footer.php
@@ -8,10 +8,10 @@
                 <a href="tel:+380937285663">(093) 728-56-63</a>
                 <a href="tel:+380987349808">(098) 734-98-08</a>
             </div>
-            <div class="footer__social" >
-                <a class="footer__social-linkedin" href="#"></a>
-                <a class="footer__social-instagram" href="#"></a>
-                <a class="footer__social-facebook" href="#"></a>
+            <div class="footer__soc" >
+                <a class="footer__soc-linkedin" href="#"></a>
+                <a class="footer__soc-instagram" href="#"></a>
+                <a class="footer__soc-facebook" href="#"></a>
             </div>
             <div class="footer__sponsor">
                 <div class="footer__sponsor-wrap" >
