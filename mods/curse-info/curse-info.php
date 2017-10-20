da703b2d859cfce3e3c18a10ddcc4daf0eea3999
Annie Alig <coolahhy@gmail.com>
2017-10-20 20:19:30 +0300

    working with info

--- mods/curse-info/curse-info.php
+++ mods/curse-info/curse-info.php
@@ -1,42 +1,46 @@
 <div class="wrapper curse-info">
-    <!-- <div class="info-wrap--lg"> -->
-        <p class="info__main info__main--color">
-            30 октября
-            <span class="info__sub">
-                старт
-            </span>
-        </p>
-    <!-- </div>
-    <div class="info-wrap--lg"> -->
-        <p class="info__main">
-            19:00-21:00
-            <span class="info__sub">
-                пн, ср, пт
-            </span>
-        </p>
-    <!-- </div>
-    <div class="info-wrap--sm"> -->
-        <p class="info__main info__main--center">
-            46
-            <span class="info__sub">
-                часов
-            </span>
-        </p>
-    <!-- </div>
-    <div class="info-wrap--sm"> -->
-        <p class="info__main info__main--center">
-            1.5
-            <span class="info__sub">
-                месяца
-            </span>
-        </p>
-    <!-- </div>
-    <div class="info-wrap--lg"> -->
-        <p class="info__main">
-            6 200 грн
-            <span class="info__sub">
-                стоимость курса
-            </span>
-        </p>
-    <!-- </div> -->
+    <div class="info-row">
+        <div class="info-wrap--lg">
+            <p class="info__main info__main--color">
+                30 октября
+                <span class="info__sub">
+                    старт
+                </span>
+            </p>
+        </div>
+        <div class="info-wrap--lg">
+            <p class="info__main">
+                19:00-21:00
+                <span class="info__sub">
+                    пн, ср, пт
+                </span>
+            </p>
+        </div>
+    </div>
+    <div class="info-row">
+        <div class="info-wrap--lg">
+            <p class="info__main info__main--center">
+                46
+                <span class="info__sub">
+                    часов
+                </span>
+            </p>
+        <!-- </div>
+        <div class="info-wrap--sm"> -->
+            <p class="info__main info__main--center">
+                1.5
+                <span class="info__sub">
+                    месяца
+                </span>
+            </p>
+        </div>
+        <div class="info-wrap--lg">
+            <p class="info__main">
+                6 200 грн
+                <span class="info__sub">
+                    стоимость курса
+                </span>
+            </p>
+        </div>
+    </div>
 </div>
