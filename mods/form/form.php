ce2c6771d25439ea06cbd2157c2e6c0005298f60
Anna Vovchenko <coolahhy@gmail.com>
2017-10-23 00:52:57 +0300

    working with form

--- mods/form/form.php
+++ mods/form/form.php
@@ -1,11 +1,6 @@
 <div class="curse-header form-img">
     <div class="curse-header_container">
-        <img class="curse__post-img" alt="Post logo image"
-            <?php $thumb_id = get_post_thumbnail_id();?>
-            src="<?php wp_get_attachment_image_url( $thumb_id ) ?>"
-            srcset="<?php echo wp_get_attachment_image_srcset( $thumb_id, 'full' ) ?>"
-            sizes="<?php echo wp_get_attachment_image_sizes( $thumb_id, 'full' ) ?>"                    
-        >
+        <img class="curse__post-img" <?php buTheme_src_set() ?> >
         <form action="" class="form">
             <label for="name" class="form__label">Имя 
                 <span class="label--required">*</span>
@@ -18,12 +13,13 @@
             <label for="tel" class="form__label">Телефон 
                 <span class="label--required">*</span>
             </label>
-            <input type="tel" class="form__input" id="tel" placeholder="+38(050)123-45-67" required>
+            <input type="tel" class="form__input" id="tel" placeholder="+38(050)123-45-67" 
+                pattern="\D[0-9]{3}\s\D[0-9]{2}\D\s[0-9]{3}\s\D\s[0-9]{2}\s\D\s[0-9]{2}" required>
             <label for="email" class="form__label">E-mail 
                 <span class="label--required">*</span>
             </label>
             <input type="email" class="form__input" id="email" placeholder="your@email.com" required>
-            <input type="submit" class="form__btn btn btn--action" value="Записаться">
+            <button type="button" class="form__btn btn btn--action">Записаться </button>
 
         </form>
     </div>				
