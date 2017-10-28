b69c7414d8fd8ebff1b130ea8f108f9d6d13b8bb
Annie Alig <coolahhy@gmail.com>
2017-10-28 22:15:38 +0300

    working with post order

--- mods/post/post.php
+++ mods/post/post.php
@@ -2,26 +2,15 @@
     <div class="post-container">
         <?php
             if ( is_page( 'programs' ) ) {
-                $args = array('posts_per_page' => 0, 'category_name' => 'programs', 'order' => 'DESC' );
+                $args = array('posts_per_page' => 0, 'category_name' => 'programs', 'meta_key'	=> 'start', 'orderby'	=> 'meta_value_num', 'order' => 'DESC' );
             }
             else {
-                $args = array('posts_per_page' => 7, 'category_name' => 'programs, events', 'order' => 'DESC' );                
+                $args = array('posts_per_page' => 7, 'category_name' => 'programs, events', 'meta_key'	=> 'start', 'orderby'	=> 'meta_value_num', 'order' => 'DESC' );                
             }
             global $post;
             $posts = get_posts( $args );
             foreach($posts as $post){ setup_postdata($post);
         ?>
-
-        <!-- <a class="tile post-single" style="background-image: url(<?php echo get_the_post_thumbnail_url() ?>);" href="<?php echo get_permalink(); ?>">
-            <span class="post__tag">Мероприятие</span>
-            <h2 class="post__title">
-                <?php echo get_the_title() ?>
-            </h2>
-            <span class="post__btn btn">
-            старт <?php echo get_post_meta( $post->ID, 'start', true ); ?>
-            <span class="post__btn--add btn">Подробнее</span></span>
-            
-        </a> -->
 
         <a class="tile post-single" style="background-image: url(<?php echo get_the_post_thumbnail_url() ?>);" href="<?php echo get_permalink(); ?>">
            <?php
