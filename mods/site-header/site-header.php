fa5034e4de48d72ea0dd5c69ba9a98c38a86323d
digvice <digvice81@gmail.com>
2017-10-29 18:51:57 +0200

    Added choosing language menu (Plugin qtranslate-x MUST be installed)

--- mods/site-header/site-header.php
+++ mods/site-header/site-header.php
@@ -1,17 +1,7 @@
 <header class="header-container">
     <div class="wrapper header">
-    <?php buTheme_custom_logo() ?>
-        <ul class="languages">
-            <li class="languages__item">
-                <a href="#rus" class="languages__is-current">Рус</a>
-            </li>
-            <li class="languages__item">
-                <a href="#ukr">Укр</a>
-            </li>
-            <li class="languages__item">
-                <a href="#eng">Eng</a>
-            </li>
-        </ul>
+        <?php buTheme_custom_logo() ?>
+        <?php qtranxf_generateLanguageSelectCode($type='text'); ?>
         <nav class="main-menu">
             <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
         </nav>
