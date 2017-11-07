aaba6f0b8e5bbc44b814562d4cd29511c8178060
Annie Alig <coolahhy@gmail.com>
2017-11-07 19:34:22 +0200

    fixed tags

--- mods/page-programs/programs.php
+++ mods/page-programs/programs.php
@@ -14,42 +14,43 @@ $html .= '<div class="wrapper tags-container">';
             'order' => 'ASC' );
         $posts = get_posts( $args );
 
+        if (count($posts) > 0){
+            $html .= '<div class="post-container">';
+            $html .= "<h2 class='tag'>";
+            $html .= "{$tag->name}</h2>";      
         
-        $html .= '<div class="post-container">';
-        $html .= "<h2 class='tag'>";
-        $html .= "{$tag->name}</h2>";
         
-        
-        foreach($posts as $post){ 
-            setup_postdata($post);
-            $post_title = get_the_title();
-            $post_thumb = get_the_post_thumbnail_url();
-            $post_link = get_permalink();
+            foreach($posts as $post){ 
+                setup_postdata($post);
+                $post_title = get_the_title();
+                $post_thumb = get_the_post_thumbnail_url();
+                $post_link = get_permalink();
 
-            $more = LangDicts::$dict['More'];
+                $more = LangDicts::$dict['More'];
 
-            $dateformatstring = "j F";
-            $unixtimestamp = strtotime(get_field('start'));
-            $date = date_i18n($dateformatstring, $unixtimestamp);
-            $full_date = date_i18n('j F Y', $unixtimestamp);
-            $year = date('Y');
-            $start_year = date_i18n('Y', $unixtimestamp);
+                $dateformatstring = "j F";
+                $unixtimestamp = strtotime(get_field('start'));
+                $date = date_i18n($dateformatstring, $unixtimestamp);
+                $full_date = date_i18n('j F Y', $unixtimestamp);
+                $year = date('Y');
+                $start_year = date_i18n('Y', $unixtimestamp);
 
-            $html .= "<a class='tile post-single' style='background-image: url({$post_thumb})' href='{$post_link}'>";
-            $html .= "<h2 class='post__title'>{$post_title}</h2>";
-            $html .= "<span class='post__btn btn'>";
-            if ($start_year === $year){
-                $html .= "{$date}";
+                $html .= "<a class='tile post-single' style='background-image: url({$post_thumb})' href='{$post_link}'>";
+                $html .= "<h2 class='post__title'>{$post_title}</h2>";
+                $html .= "<span class='post__btn btn'>";
+                if ($start_year === $year){
+                    $html .= "{$date}";
+                }
+                else{
+                    $html .= "{$full_date}";
+                }
+                $html .= "<span class='post__btn--add btn'>{$more}</span>";
+                $html .= "</span></a>";
             }
-            else{
-                $html .= "{$full_date}";
-            }
-            $html .= "<span class='post__btn--add btn'>{$more}</span>";
-            $html .= "</span></a>";
+            $html .= '</div>';
+
+            wp_reset_postdata();
         }
-        $html .= '</div>';
-
-        wp_reset_postdata();
     }
     $html .= '</div>';
     echo $html; 
