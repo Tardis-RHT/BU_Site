0ecd2e21973425088fb159e4dd66cc853c5dd92e
Annie Alig <coolahhy@gmail.com>
2017-11-08 19:49:30 +0200

    page projects in progress (ukr)

--- mods/page-projects/projects.php
+++ mods/page-projects/projects.php
@@ -8,7 +8,9 @@
                 <div class="project-info">
                     <h2 class="project__title">BU Dev Studio</h2>
                     <p class="project__about">BU DevStudio – комерційний проект з розробки програмного забезпечення, а також один з майданчиків для працевлаштування випускників програм і проектів BIONIC. При BU DevStudio функціонує інтернатура, в якій кращі випускники BIONIC School поглиблюють свої знання з технологій і знайомляться з основами командної розробки на реальних проектах.</p>
-                    <span class="project__more btn">Детальніше</span>
+                    <span class="project__more btn">
+                        <?php echo LangDicts::$dict['More']; ?>
+                    </span>
                 </div>
             </a>
             <a href="#" class="single-project">
@@ -19,7 +21,9 @@
                     <h2 class="project__title">BIONIC Summer Camp</h2>
                     <p class="project__about">Summer Camp – це освітня інтенсивна програма від BIONIC School, яка дозволяє учасникам отримати необхідні знання в обраній IT-технології, засвоїти та практично застосувати їх працюючи в команді над справжнім проектом, а також пройти низку програм та тренінгів з розвитку особистісних навичок та компетенцій.
                         BIONIC Summer Camp - твій перший крок на шляху до успішної кар’єри в IT-індустрії.</p>
-                    <span class="project__more btn">Детальніше</span>
+                    <span class="project__more btn">
+                        <?php echo LangDicts::$dict['More']; ?>
+                    </span>
                 </div>
             </a>
             <a href="#" class="single-project">
@@ -30,7 +34,9 @@
                     <h2 class="project__title">Data Science</h2>
                     <p class="project__about">Data Science – це освітня програма від BIONIC School, у межах якої учасники навчаться працювати з великими даними та використовувати Python для аналізу даних та збору статистики.
                         BIONIC Summer Camp - твій перший крок на шляху до успішної кар’єри в IT-індустрії.</p>
-                    <span class="project__more btn">Детальніше</span>
+                    <span class="project__more btn">
+                        <?php echo LangDicts::$dict['More']; ?>
+                    </span>
                 </div>
             </a>
         </div>
