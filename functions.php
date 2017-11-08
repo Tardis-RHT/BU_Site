505aaa1704c92d0649a969977b1828d5c9d0561f
Annie Alig <coolahhy@gmail.com>
2017-11-08 22:16:28 +0200

    started working with projects

--- functions.php
+++ functions.php
@@ -460,6 +460,45 @@ if(function_exists("register_field_group"))
 		'menu_order' => 0,
 	));
 }
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_projects',
+		'title' => '[:en]projects[:]',
+		'fields' => array (
+			array (
+				'key' => 'field_5a0363234fbdd',
+				'label' => 'link',
+				'name' => 'link',
+				'type' => 'text',
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'formatting' => 'html',
+				'maxlength' => '',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_category',
+					'operator' => '==',
+					'value' => buTheme_slugid('projects'),
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'default',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
 
 
 /*------------------------------------*\
