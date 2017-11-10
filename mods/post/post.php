9b262b0c211ce22a738761d565a7712da7cf3129
digvice <digvice81@gmail.com>
2017-11-10 20:55:24 +0200

    111

--- mods/post/post.php
+++ mods/post/post.php
@@ -5,79 +5,68 @@
         
         if (in_category('news')) echo '<h2 class="tag">' . LangDicts::$dict['curse_nabor_text'] . '</h2>';
                
-            global $post;
-            $today = date('Ymd');
-            $tag_ids = wp_get_post_tags( $post->ID, array( 'fields' => 'ids' ) );
-            if(in_category('news') ){
-                $args = array(
-                    'posts_per_page' => 4, 
-                    'category_name' => 'programs',
-                    'tag__in' => $tag_ids,
-                    'meta_key'	=> 'start',
-                    'meta_type' => 'DATETIME',
-                    'meta_compare' => '>=',
-                    'meta_value' => $today,
-                    'orderby' => 'meta_value', 
-                    'order' => 'ASC' );
-            }
-            elseif(in_category( 'programs' ) || in_category( 'events' ) ){
-                $args = array(
-                    'posts_per_page' => 4, 
-                    'category_name' => 'programs, events',
-                    'post__not_in' => array( $post->ID ),
-                    'meta_key'	=> 'start',
-                    'meta_type' => 'DATETIME',
-                    'meta_compare' => '>=',
-                    'meta_value' => $today,
-                    'orderby' => 'meta_value', 
-                    'order' => 'ASC' );
-            }
-            // elseif(is_page( 'events' ) ){
-            //     $args = array(
-            //         'posts_per_page' => 4, 
-            //         'category_name' => 'programs, events',
-            //         'post__not_in' => array( $post->ID ),
-            //         'meta_key'	=> 'start',
-            //         'meta_type' => 'DATETIME',
-            //         'meta_compare' => '>=',
-            //         'meta_value' => $today,
-            //         'orderby' => 'meta_value', 
-            //         'order' => 'ASC' );
-            // }
-            else $args = array(
-                    // 'posts_per_page' => 7, 
-                    // 'category_name' => 'programs, events',
-                    // 'order' => 'ASC',
-                    // 'orderby' => 'meta_value', 
-                    // 'meta_key'	=> 'current', 
-                    // 'meta_compare' => '==',
-                    // 'meta_value' => '1',
-                    'posts_per_page' => 7, 
-                    'category_name' => 'programs, events',
-                    'meta_key'	=> 'start', 
-                    'meta_type' => 'DATETIME',
-                    'meta_compare' => '>=',
-                    'meta_value' => $today,
-                    'order' => 'ASC',                
-             );
-          
-            $posts = get_posts( $args);
-            // if (count($posts) < 3) {
-            //     // while(count($posts) < 4) { 
-            //     $args = array(
-            //         'posts_per_page' => 7, 
-            //         'category_name' => 'programs, events',
-            //         'meta_key'	=> 'start', 
-            //         'meta_type' => 'DATETIME',
-            //         'meta_compare' => '>=',
-            //         'meta_value' => $today,
-            //         'order' => 'ASC',
-            //     );
+        global $post;
+        $today = date('Ymd');
+        $tag_ids = wp_get_post_tags( $post->ID, array( 'fields' => 'ids' ) );
+        // echo count($tag_ids);
+        if(in_category('news') ){
+            $args = array(
+                'posts_per_page' => 4, 
+                'category_name' => 'programs',
+                'tag__in' => $tag_ids,
+                'meta_key'	=> 'start',
+                'meta_type' => 'DATETIME',
+                'meta_compare' => '>=',
+                'meta_value' => $today,
+                'orderby' => 'meta_value', 
+                'order' => 'ASC' );
+        }
+        elseif(in_category( 'programs' ) || in_category( 'events' ) ){
+            $args = array(
+                'posts_per_page' => 4, 
+                'category_name' => 'programs, events',
+                'post__not_in' => array( $post->ID ),
+                'meta_key'	=> 'start',
+                'meta_type' => 'DATETIME',
+                'meta_compare' => '>=',
+                'meta_value' => $today,
+                'orderby' => 'meta_value', 
+                'order' => 'ASC' );
+        }
+        else $args = array(
+                // 'posts_per_page' => 7, 
+                // 'category_name' => 'programs, events',
+                // 'order' => 'ASC',
+                // 'orderby' => 'meta_value', 
+                // 'meta_key'	=> 'current', 
+                // 'meta_compare' => '==',
+                // 'meta_value' => '1',
+                'posts_per_page' => 7, 
+                'category_name' => 'programs, events',
+                'meta_key'	=> 'start', 
+                'meta_type' => 'DATETIME',
+                'meta_compare' => '>=',
+                'meta_value' => $today,
+                'order' => 'ASC',                
+            );
+
+        $posts = get_posts($args);
+        // if (count($posts) < 4) {
+        //     // while(count($posts) < 4) { 
+        //     $args = array(
+        //         'posts_per_page' => 1, 
+        //         'category_name' => 'programs, events',
+        //         'meta_key'	=> 'start', 
+        //         'meta_type' => 'DATETIME',
+        //         'meta_compare' => '>=',
+        //         'meta_value' => $today,
+        //         'order' => 'ASC',
+        //     );
+        //         $posts = get_posts($args); 
+        //     // }
+        // }          
+        foreach($posts as $post){ setup_postdata($post);
                 
-            //         $posts = get_posts($args); 
-            //     // }
-            // }             
-            foreach($posts as $post){ setup_postdata($post);
         ?>
         <a class="tile post-single <?php if (is_front_page()) echo 'post-single--big'; ?>" style="background-image: url(<?php echo get_the_post_thumbnail_url() ?>);" href="<?php echo get_permalink(); ?>">
        
