a33d03502faccac6b09aad0ba90146766f432ccb
digvice <digvice81@gmail.com>
2017-11-13 17:09:34 +0200

    Added links to social media in footer

--- mods/site-footer/site-footer.php
+++ mods/site-footer/site-footer.php
@@ -12,9 +12,9 @@
                 <a class="footer_tel" href="tel:+380987349808">(098) 734-98-08</a>
             </div>
             <div class="footer__soc">
-                <a class="soc-link" href="#"><svg class="soc_icon"><use xlink:href="#linkedin"></use></svg><span class="nostyle">LinkedIn</span></a>
-                <a class="soc-link" href="#"><svg class="soc_icon"><use xlink:href="#instagram"></use></svg><span class="nostyle">Instagram</span></a>
-                <a class="soc-link" href="#"><svg class="soc_icon"><use xlink:href="#facebook"></use></svg><span class="nostyle">Facebook</span></a>
+                <a class="soc-link" href="#"><svg class="soc_icon"><use xlink:href="https://www.linkedin.com/company/bionic-university?trk=prof-following-company-logo"></use></svg><span class="nostyle">LinkedIn</span></a>
+                <a class="soc-link" href="#"><svg class="soc_icon"><use xlink:href="https://www.instagram.com/bionic_university/"></use></svg><span class="nostyle">Instagram</span></a>
+                <a class="soc-link" href="#"><svg class="soc_icon"><use xlink:href="https://www.facebook.com/BionicUniversity"></use></svg><span class="nostyle">Facebook</span></a>
             </div>
             <div class="footer__block footer__sponsor">
                 <div class="footer__sponsor-wrap" >
