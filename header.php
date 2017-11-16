f322895927c27610014b75dea7c47acbdeff9512
digvice <digvice81@gmail.com>
2017-11-16 15:32:56 +0200

    Added "Google Tag Manager" in header.php and index.php

--- header.php
+++ header.php
@@ -4,7 +4,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link href="<?php echo get_template_directory_uri(); ?>/css/stylemin.css" rel="stylesheet" />
 	<link href="<?php echo get_template_directory_uri(); ?>/css/fonts.css" rel="stylesheet" />
-
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?...;
+	})(window,document,'script','dataLayer','GTM-N5WW265');</script>
+	<!-- End Google Tag Manager -->
 	<!-- add custom css for IE -->
 	<?php
 	$user_agent = $_SERVER["HTTP_USER_AGENT"];
