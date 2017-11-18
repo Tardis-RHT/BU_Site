af2a855f64a8da5bba65a9e17e7377a28cf0b89b
Annie Alig <coolahhy@gmail.com>
2017-11-18 19:14:33 +0200

    deleted variables from width

--- mods/trainers/trainers.php
+++ mods/trainers/trainers.php
@@ -1,10 +1,9 @@
 <?php if (get_field('teacher')){ ?>
 
-<!-- <p><?php echo LangDicts::$dict['teachers'] ?></p>-->
-<?php echo $teacher ?>
 <div class="trainers-container">
     <?php $teachers = get_field('teacher');
         foreach($teachers as $teacher){
+            // print_r($teacher);
             echo '<div class="trainer">' 
                 . $teacher[user_avatar] . '
                 <span class="trainer__position">' . LangDicts::$dict['teacher'] . '</span>
@@ -13,15 +12,6 @@
                 <div class="trainer__about">' . $teacher[user_description] . '
                 </div>
             </div>';
-    // <div class="trainer">
-    //     <img class="trainer__photo" src="https://secure.gravatar.com/avatar/ace8e80e05aaf36e3752d8a202ec87c3?s=96&d=mm&r=g" alt="">
-    //     <span class="trainer__position">Тренер</span>
-    //     <h3 class="trainer__name">Константин Константиновский</h3>
-    //     <p class="trainer__work">Software Engineer @ Infopulse</p>
-    //     <div class="trainer__about">
-    //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quia cumque vero autem cum, obcaecati eos itaque quasi! Quos ea, excepturi voluptatum magni sint nam totam minima repudiandae natus omnis.
-    //     </div>
-    // </div>
         }
     ?>
 </div>
