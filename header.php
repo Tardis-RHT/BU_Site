f200ba105949227c858134b22696266b59588456
Konakov Roman <konakov.r.m@gmail.com>
2017-11-22 01:06:02 +0200

    Updated meta viewport in header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <head>
 	<title><?php wp_title(''); if ( is_single() ) echo ' — ', bloginfo('name'); ?></title>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<link href="<?php echo get_template_directory_uri(); ?>/css/stylemin.css" rel="stylesheet" />
 	<link href="<?php echo get_template_directory_uri(); ?>/css/fonts.css" rel="stylesheet" />
 	<!-- Google Tag Manager -->
