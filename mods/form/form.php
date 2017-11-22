3652f0e0741df3c88b2e7a3a2dcd90b181468b5b
digvice <digvice81@gmail.com>
2017-11-22 16:53:32 +0200

    Added "name" for form

--- mods/form/form.php
+++ mods/form/form.php
@@ -5,38 +5,36 @@
             action="<?php echo get_template_directory_uri(); ?>/thankyou/" 
             method="GET"
             target="_blank"
-
             class="form" id="mainForm">
-        <div>
-            <label for="name" class="form__label">
-                <?php echo LangDicts::$dict['Name'];?>
-            </label>
-            <input type="text" class="form__input" id="name" 
-                placeholder="<?php echo LangDicts::$dict['exp_name'];?>" required>
-        </div>    
-        <div>
-            <label for="surname" class="form__label">
-                <?php echo LangDicts::$dict['Surname'];?>
-            </label>
-            <input type="text" class="form__input" id="surname"
-                placeholder="<?php echo LangDicts::$dict['exp_surname'];?>" required>
-        </div>
-        <div>
-            <label for="tel" class="form__label">
-                <?php echo LangDicts::$dict['Phone'];?>
-            </label>
-            <input type="tel" class="form__input" id="tel" placeholder="+38(___)___-__-__" 
-                pattern="\D[0-9]{2}\s\D[0-9]{3}\D\s[0-9]{3}\s\D\s[0-9]{2}\s\D\s[0-9]{2}" required>
-        </div>
-        <div>
-            <label for="email" class="form__label">
-            <?php echo LangDicts::$dict['Email'];?>
-            </label>
-            <input type="email" class="form__input" id="email" placeholder="your@email.com" required>
-        </div>
-        <input type="text" hidden value="<?php echo get_the_title() ?>">
-            <button type="submit" class="form__btn btn btn--action">
-                <?php echo LangDicts::$dict['Apply'];?> </button>
+            <div>
+                <label for="name" class="form__label">
+                    <?php echo LangDicts::$dict['Name'];?>
+                </label>
+                <input type="text" class="form__input" id="name" name="name"
+                    placeholder="<?php echo LangDicts::$dict['exp_name'];?>" required>
+            </div>    
+            <div>
+                <label for="surname" class="form__label">
+                    <?php echo LangDicts::$dict['Surname'];?>
+                </label>
+                <input type="text" class="form__input" id="surname" name="surname"
+                    placeholder="<?php echo LangDicts::$dict['exp_surname'];?>" required>
+            </div>
+            <div>
+                <label for="tel" class="form__label">
+                    <?php echo LangDicts::$dict['Phone'];?>
+                </label>
+                <input type="tel" class="form__input" id="tel" name="tel" placeholder="+38(___)___-__-__" 
+                    pattern="\D[0-9]{2}\s\D[0-9]{3}\D\s[0-9]{3}\s\D\s[0-9]{2}\s\D\s[0-9]{2}" required>
+            </div>
+            <div>
+                <label for="email" class="form__label">
+                <?php echo LangDicts::$dict['Email'];?>
+                </label>
+                <input type="email" class="form__input" id="email" name="email" placeholder="your@email.com" required>
+            </div>
+            <input type="text" hidden value="<?php echo get_the_title() ?>" name="title" id="title">
+            <button type="submit" class="form__btn btn btn--action"><?php echo LangDicts::$dict['Apply'];?> </button>
         </form>
     </div>				
 </div>
